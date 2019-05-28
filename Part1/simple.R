25e0d3157eb137d4fef0381c4921c4e162f68c94
lgeistlinger <ludwig.geistlinger@bio.ifi.lmu.de>
2019-05-28 12:09:58 -0400

    add exercises

---
+++ Part1/simple.R
@@ -0,0 +1,28 @@
+1 + 2
+
+c(1, 3, 5)
+
+1:5
+
+x <- c(1, 3, 5)
+x
+
+log(x)
+
+y <- log(x)
+y
+
+fname <- file.choose()
+fname
+
+file.exists(fname)
+
+pdata <- read.csv(fname)
+dim(pdata) 
+
+head(pdata)
+tail(pdata)
+summary(pdata)
+
+
+
